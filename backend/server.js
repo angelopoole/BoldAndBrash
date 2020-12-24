2dc85114fb0c39ffd1494d1d24ce76e58bde7c01
angelopoole <angelo.poole@outlook.com>
2020-12-23 23:44:24 -0500

    init project structure

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import artworks from './data/artworks.js';
+
+const app = express(artworks);
+
+app.get('/', (req, res) => {
+	res.send('API IS RUNNING');
+});
+
+app.get('/artworks', (req, res) => {
+	res.send(artworks);
+});
+
+app.listen(5000, console.log('Server running on port 5000'));
